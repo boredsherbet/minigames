923c175b74c9c19014a3074204fded5b72ae64bd
Christian <ccantony064@gmail.com>
2022-03-26 20:32:57 -0500

    commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const compChoiceDisp = document.getElementById("compChoice");
+const userChoiceDisp = document.getElementById("userChoice");
+const result = document.getElementById("result");
+const possibleChoices = document.querySelectorAll("button");
+let userChoice;
+possibleChoices.forEach(possibleChoice).addEventListener(click, (e)=> {
+    userChoice = e.target.id
+    userChoiceDisp.innerHTML = userChoice;
+})
