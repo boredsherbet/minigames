2c596481e36a1006915e2b4db726b95466f971d3
Christian <ccantony064@gmail.com>
2022-03-26 22:01:01 -0500

    set up homepage & finished rps

--- app.js
+++ app.js
@@ -3,14 +3,15 @@ const userChoiceDisp = document.getElementById("userChoice");
 const result = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
 let userChoice;
-let compChoice
 possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e)=> {
     userChoice = e.target.id;
     userChoiceDisp.innerHTML = userChoice;
     chooseMove();
+    getresult();
 }));
 function chooseMove(){
-    const randomNum = Math.floor(Math.random() * possibleChoices.length()) +1;
+    console.log("duck.");
+    const randomNum = Math.floor(Math.random() * possibleChoices.length) +1;
     switch(randomNum){
         case 1: 
         compChoice = "rock";
@@ -22,6 +23,14 @@ function chooseMove(){
         compChoice = "paper";
         break;
     }
-    compChoiceDisp.innerHTML = compChoice;
-
 }
+function getresult(){
+        if(compChoice == userChoice){
+            result.innerHTML = "draw.:|"
+        } 
+        else if((compChoice=="rock"&&userChoice=="paper") || (compChoice=="paper" && userChoice=="scissors") || (compChoice=="scissors" && userChoice=="rock")){
+            result.innerHTML= "you won! :)";
+        } else{
+            result.innerHTML="you lose. :("
+        }
+    }
