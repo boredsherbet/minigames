64b618b1123c1448048968862cfdc669c65f48a0
Christian <ccantony064@gmail.com>
2022-03-26 20:53:35 -0500

    mo

--- app.js
+++ app.js
@@ -3,7 +3,23 @@ const userChoiceDisp = document.getElementById("userChoice");
 const result = document.getElementById("result");
 const possibleChoices = document.querySelectorAll("button");
 let userChoice;
-possibleChoices.forEach(possibleChoice).addEventListener(click, (e)=> {
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e)=> {
     userChoice = e.target.id;
     userChoiceDisp.innerHTML = userChoice;
-})
+    
+}));
+function chooseMove(){
+    const randomNum = Math.floor(Math.random() * possibleChoices.length()) +1;
+    switch(randomNum){
+        case 1: 
+        compChoiceDisp.innerHTML = "rock";
+        break;
+        case 2: 
+        compChoiceDisp.innerHTML = "scissors";
+        break;
+        default: 
+        compChoiceDisp.innerHTML = "paper";
+        break;
+    }
+
+}
