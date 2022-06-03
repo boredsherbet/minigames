53bb64f8135937f5a774d9052574f762741c8beb
Christian <ccantony064@gmail.com>
2022-06-02 21:07:55 -0500

    setup node thing?

--- learningReact/Footer.js
+++
@@ -1,7 +0,0 @@
-export default function Footer() {
-  return (
-    <footer className="footer">
-      <small>Copyright 2022 sherbet bein' bored. All rights reserved. </small>
-    </footer>
-  );
-}
